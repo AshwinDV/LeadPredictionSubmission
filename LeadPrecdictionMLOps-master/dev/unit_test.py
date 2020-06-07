8ad672032f8182bfd9507ff7c76a947e45fcd13b
AshwinDV <ashwindv75@gmail.com>
2020-06-07 13:51:56 +0530

    Update unit_test.py

--- LeadPrecdictionMLOps-master/dev/unit_test.py
+++ LeadPrecdictionMLOps-master/dev/unit_test.py
@@ -1,5 +1,7 @@
 from app import index
+from validation import validation_test
 
 
 def test_index():
     assert index() == "<h1>Lead conversion prediction app</h1>"
+    assert validation_test() > 0.6
